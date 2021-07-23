09e70bd593d7c2fdb57b6e2e4165f81a12304f69
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-23 11:53:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -43,7 +43,6 @@ Developer is Super Noob. Just Learning from Official Docs. Please Donate the dev
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
