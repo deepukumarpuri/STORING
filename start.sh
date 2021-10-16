629c85060a9e251e552aac56a4b9d5137ecd0ac3
Dhhonu <89118494+Dhhonu@users.noreply.github.com>
2021-10-17 03:21:18 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo, Please Wait..."
+git clone https://github.com/deepukumarpuri/STORING /STORING
+cd /STORING
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
